e30ff3f6b9c35d0b326ed05b42f7c99be952d038
210366569 <101373843+210366569@users.noreply.github.com>
2022-10-10 17:37:25 +0800

    Do some ui stuff

--- Group4_FYP/Assets/Scripts/PauseMenu.cs
+++ Group4_FYP/Assets/Scripts/PauseMenu.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
+using DG.Tweening;
 
 public class PauseMenu : MonoBehaviour
 {
+    private bool bIsOpened = false;
+    [SerializeField] private GameObject sideMenuPanel;
+    [SerializeField] private GameObject settingsMenuPanel;
+    [SerializeField] private GameObject pauseMenuBackground;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,16 @@ public class PauseMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (bIsOpened)
+            {
+
+            }
+            else
+            {
+                sideMenuPanel.GetComponent<RectTransform>().DOAnchorPos(new Vector2(0, 0), 0.5f).SetEase(Ease.OutQuad);
+            }
+        }
     }
 }
