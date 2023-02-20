c49c730ff77c363a1d8410d5ffa6ed6783314674
CheungHauYung <101487936+CheungHauYung@users.noreply.github.com>
2023-02-20 17:00:16 +0800

    IDragHandler
    IDragHandler

--- Group4_FYP/Assets/Scripts/getTozTip.cs
+++ Group4_FYP/Assets/Scripts/getTozTip.cs
@@ -1,46 +1,74 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
+//using static System.Net.Mime.MediaTypeNames;
 
-public class getTozTip : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerMoveHandler, IPointerDownHandler, IPointerUpHandler
+public class getTozTip : MonoBehaviour, IDragHandler, IPointerDownHandler, IPointerUpHandler
 {
     public RectTransform canvas;
+    Vector2 canvasPos;
     private RectTransform rectTransform;
-    private bool mouseOver, mouseFollow;
+    private bool mouseDown;
+    private GameObject target;
+    private string n_target;
+    private Image t_Image;
+    private Image s_Image;
 
     public void OnPointerDown(PointerEventData eventData)
     {
-        if (!mouseOver || eventData.button != PointerEventData.InputButton.Left)
-            return;
-
-        mouseFollow = true;
+        mouseDown = (eventData.button == PointerEventData.InputButton.Left);
     }
 
     public void OnPointerUp(PointerEventData eventData)
     {
-        if (!mouseFollow || eventData.button != PointerEventData.InputButton.Left)
-            return;
-
-        mouseFollow = false;
+        if (mouseDown && eventData.button == PointerEventData.InputButton.Left)
+            mouseDown = false;
     }
 
-    public void OnPointerEnter(PointerEventData eventData)
+    public void OnDrag(PointerEventData eventData)
     {
-        mouseOver = true;
-    }
-
-    public void OnPointerExit(PointerEventData eventData)
-    {
-        mouseOver = false;
-    }
-
-    public void OnPointerMove(PointerEventData eventData)
-    {
-        RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas, eventData.position, null, out Vector2 canvasPos);
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas, eventData.position, null, out canvasPos);
         rectTransform.anchoredPosition = canvasPos;
     }
 
     private void Awake()
     {
         rectTransform = GetComponent<RectTransform>();
+    }
+
+    private void Update()
+    {
+        if (mouseDown || target == null)
+            return;
+
+        var pushToy = target.gameObject.GetComponent<pushToy>();
+        if (pushToy != null)
+        {
+            t_Image.sprite = s_Image.sprite;
+            pushToy.OnPush(gameObject);
+
+            // DEMO ONLY PLEASE REMOVE
+            gameObject.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        target = collider.gameObject;
+        n_target = collider.name;
+        t_Image = GameObject.Find(n_target + "/Image").GetComponent<Image>();
+        s_Image = GetComponent<Image>();
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (target != null)
+        {
+            target = null;
+            n_target = null;
+            t_Image = null;
+            s_Image = null;
+        }
     }
 }
